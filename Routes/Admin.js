3c35f85413b5088216c182be03c45b74ae124dc4
Xyro <monarchistt1@gmail.com>
2024-08-26 12:56:28 -0500

    Commit 2

--- Routes/Admin.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/staff/admin', (req, res) => {
-  res.render('admin', { title: 'Admin Panel' });
-});
-
-module.exports = router;
